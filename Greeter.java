711908c21c980bf1c1073bb7e311c0d41498ac61
Francis Ocoma <focoma@gmail.com>
2017-01-27 13:18:32 +0800

    Implement internationalization

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,24 @@
+package test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Greeter {
+	ResourceBundle greetings;
+
+	public Greeter() {
+		this(Locale.getDefault());
+	}
+	
+	public Greeter(Locale locale) {
+		greetings = ResourceBundle.getBundle("GreetingsBundle", locale);
+	}
+	
+	public String hello() {
+		return greetings.getString("hello");
+	}
+	
 	public static void main(String args[]) {
-		System.out.println("Hello world!");
+		System.out.println(new Greeter(Locale.ENGLISH).hello());
 	}
 }
